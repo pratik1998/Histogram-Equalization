403b5e779e6b6389c7c3d34079ca0c5f3767f3e3
pratik1998 <pratikkayastha98@gmail.com>
2018-11-05 18:42:50 +0530

    Final Histogram updated

--- HistogramFinal.cu
+++ HistogramFinal.cu
@@ -83,8 +83,8 @@ int main(int argc, char** argv)
     int sum = 0;
     for(int i=0;i<1024*256;i++)
     {
-        //for(int j=0;j<256;j++)
-        sum+=h_histogram[i*256+j];
+        for(int j=0;j<256;j++)
+            sum+=h_histogram[i*256+j];
     }
     printf("Total sum: %d\n",sum);
     /*
